eb907af6642b554386b7b4e6a5cf3c7a0643cf8f
tejass <tejassawant5325@gmail.com>
2023-02-21 20:00:00 +0530

    Added Endpoints in VillaNumberApi Controller GET,POST,PUT,DELETE(Basic Level)

--- VillaApi/Controllers/VillaNumberApiController.cs
+++ VillaApi/Controllers/VillaNumberApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using VillaApi.Data;
 using VillaApi.Models;
 using VillaApi.Models.DTO.VillaDTOs;
@@ -26,7 +27,11 @@ namespace VillaApi.Controllers
             return Ok(villaNumbers);
         }
 
+
+        // Post VillaNumbers
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<VillaNumberDTO> CreateVillaNumbers([FromBody] VillaNumberCreateDTO villaNumberCreateDTO)
         {
             if (villaNumberCreateDTO == null)
@@ -50,6 +55,73 @@ namespace VillaApi.Controllers
             return Ok(villaNumber);
 
         }
+
+        // Get Single VillaNumer
+        [HttpGet("{villaNo}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaNumberDTO> GetOneVillaNumber(int villaNo)
+        {
+            if (villaNo == 0)
+            {
+                return BadRequest();
+            }
+            var villa = _context.VillaNumbers.FirstOrDefault(u => u.VillaNo == villaNo);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+        // Delete VillaNumber
+        [HttpDelete("{villaNo}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public ActionResult DeleteVillaNumber(int villaNo)
+        {
+            if (villaNo == 0)
+            {
+                return BadRequest();
+            }
+            var villaNumber = _context.VillaNumbers.FirstOrDefault(u => u.VillaNo == villaNo);
+            if (villaNumber == null)
+            {
+                return NotFound();
+            }
+            _context.VillaNumbers.Remove(villaNumber);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        // Update VillaNumber Using PUT
+        [HttpPut("{villaNo}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult UpdateVillaNumber(int villaNo, [FromBody] VillaNumberUpdateDTO villaNumberUpdateDTO)
+        {
+            if (villaNumberUpdateDTO == null || villaNumberUpdateDTO.VillaNo != villaNo)
+            {
+                return BadRequest();
+            }
+            var villa = _context.VillaNumbers.AsNoTracking().FirstOrDefault(u => u.VillaNo == villaNo);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            VillaNumber villaNumber = new()
+            {
+                VillaNo = villaNumberUpdateDTO.VillaNo,
+                SpecialDetails = villaNumberUpdateDTO.SpecialDetails,
+                UpdatedDate = DateTime.Now
+            };
+            _context.VillaNumbers.Update(villaNumber);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
 
     }
 }
